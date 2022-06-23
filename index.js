29fda41afa6b39535ab1410446f02e1d593aa981
Federico Luna <fedelunacjs@gmail.com>
2022-06-22 23:11:08 -0300

    se corrigen errores

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-let nombre = prompt("igresa tu nombre");
-let apellido = prompt("ingresa tu apellido");
-let edad = parseInt(prompt("ingresa tu edad"));
+function PedirDatos(nombre, apellido, edad) {
+    let nombre = prompt("igresa tu nombre");
+    let apellido = prompt("ingresa tu apellido");
+    let edad = parseInt(prompt("ingresa tu edad"));
 
-let datos = (`${nombre} ${apellido}, Tu edad es ${edad} `)
+    alert = (`${nombre} ${apellido}, Tu edad es ${edad} `)
+}
+
+PedirDatos(nombre);
+PedirDatos(apellido);
+PedirDatos(edad);
 
 if(edad > 17){
     alert(datos);
@@ -13,4 +19,7 @@ alert("Edad Insuficiente")
 edad = prompt("Volve a ingresar tu edad");
 }
 
+nombre = "fede";
+apellido = "luna";
+edad = 33;
 
